47485d6666475bf6f89db47e2d73d3475d4ca103
HiroUrata <1712026@i.softbank.jp>
2021-07-19 21:08:21 +0900

    TextFieldの値でcellの個数を決めていたのを、UISliderのvalueによってcellの個数を決めるに変更

--- UICollectionViewLessonApp/ViewController.swift
+++ UICollectionViewLessonApp/ViewController.swift
@@ -9,19 +9,23 @@ import UIKit
 
 class ViewController: UIViewController{
 
-    @IBOutlet weak var cellCountTextField: UITextField!
-    @IBOutlet weak var decisionButton: UIButton!
+   
     @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var cellCountSlider: UISlider!
     
     let collectionViewFlowLayout = UICollectionViewFlowLayout()
     
-    var cellCount = String()
+    var cellcount = 0
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        cellCountSlider.minimumValue = 0
+        cellCountSlider.maximumValue = 100
         
+        collectionView.delegate = self
+        collectionView.dataSource = self
 
     }
 
@@ -29,22 +33,24 @@ class ViewController: UIViewController{
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        collectionViewFlowLayout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        collectionViewFlowLayout.sectionInset = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
         collectionView.collectionViewLayout = collectionViewFlowLayout
         
+        
     }
     
-    
-    @IBAction func countDecision(_ sender: Any) {
+    @IBAction func cellCountUpMinus(_ sender: UISlider) {
         
-        collectionView.delegate = self
-        collectionView.dataSource = self
+        if sender.value >= 0{
+            
+            cellcount = Int(sender.value)
+            collectionView.reloadData()
+            
+        }
         
-        cellCount = cellCountTextField.text!
-        
-        collectionView.reloadData()
     }
     
+  
 }
 
 
@@ -55,7 +61,7 @@ extension ViewController:UICollectionViewDelegate,UICollectionViewDataSource,UIC
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         
         
-        return Int(cellCount)!
+      return cellcount
         
     }
     
